ec579e8a685451561040c9488f7c5383d42135fb
Pedro from Macbook <pfernandezgonzalez@vva.safanet.es>
2024-01-20 21:10:42 +0100

    Version 0.4.5.1 - Corrección de errores

--- app/src/main/java/com/example/aplicacionfragmentos/ui/slideshow/AjustesFragment.kt
+++ app/src/main/java/com/example/aplicacionfragmentos/ui/slideshow/AjustesFragment.kt
@@ -7,18 +7,18 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.aplicacionfragmentos.R
-import com.example.aplicacionfragmentos.databinding.FragmentSlideshowBinding
+import com.example.aplicacionfragmentos.databinding.FragmentAjustesBinding
 
-class SlideshowFragment : Fragment() {
+class AjustesFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentAjustesBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentAjustesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         setupSwitch(binding.switchNotifications)
